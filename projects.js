14a94e5ed6c9ffa1cc9bcdc4cb05b6c0c7b8a888
FCR-Official <bradbot2020@gmail.com>
2026-07-12 21:26:11 -0400

    Implement project creation and display functionality
    Added functions to create and display projects using localStorage.

--- projects.js
+++ projects.js
@@ -1,1 +1,123 @@
+let projects =
+JSON.parse(
+localStorage.getItem("projects")
+)
+|| [];
 
+
+const projectList =
+document.getElementById("projectList");
+
+
+
+function createProject(){
+
+
+let name =
+document.getElementById("projectName").value;
+
+
+let description =
+document.getElementById("projectDescription").value;
+
+
+
+let project = {
+
+name:name,
+
+description:description,
+
+creator:"User",
+
+shared:true
+
+};
+
+
+
+projects.push(project);
+
+
+
+localStorage.setItem(
+
+"projects",
+
+JSON.stringify(projects)
+
+);
+
+
+
+displayProjects();
+
+}
+
+
+
+function displayProjects(){
+
+
+projectList.innerHTML="";
+
+
+
+projects.forEach(project=>{
+
+
+let card =
+document.createElement("div");
+
+
+card.className =
+"project-card";
+
+
+
+card.innerHTML =
+
+`
+
+<h2>
+${project.name}
+</h2>
+
+<p>
+${project.description}
+</p>
+
+
+<small>
+Created by ${project.creator}
+</small>
+
+`;
+
+
+
+projectList.appendChild(card);
+
+
+});
+
+
+}
+
+
+
+function goHome(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+}
+
+
+
+displayProjects();
